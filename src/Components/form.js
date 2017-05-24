72bb70ab17a2b229600336c82579377db0ae61ee
nightingale69 <jaccobdasouza@gmail.com>
2017-05-24 22:06:25 +0300

    У каждого input свой обработчик, в constructor добавлены стейты

--- src/Components/form.js
+++ src/Components/form.js
@@ -2,20 +2,63 @@ import React from 'react';
 import './Styles/form.css';
 
 class Form extends React.Component {
-    constructor(){
+    constructor() {
         super();
-        this.state = {surname: ''};
-        this.state = {name: ''};
+            this.state = {
+                surname:'', name:'', secondname:'', 
+                begin:'', end:'', term:'', date:''
+            };
     }
-    update(){
-        console.log('Фамилия:', this.refs.surname.value);
-        console.log('Имя:', this.refs.name.value);
-        console.log('Отчество:', this.refs.secondname.value);
-        console.log('Начало отпуска:', this.refs.begin.value);
-        console.log('Окончание отпуска:', this.refs.end.value);
-        console.log('Срок:', this.refs.term.value);
-        console.log('Дата заявления:', this.refs.date.value);
-    }
+    
+    handleInputBlur1 = (e) => {
+        this.setState({
+            surname: e.target.value
+        });
+        console.log('Фамилия:', e.target.value);
+    };
+    
+    handleInputBlur2 = (e) => {
+        this.setState({
+            name: e.target.value
+        });
+        console.log('Имя:', e.target.value);
+    };
+    
+    handleInputBlur3 = (e) => {
+        this.setState({
+            secondname: e.target.value
+        });
+        console.log('Отчество:', e.target.value);
+    };
+        
+    handleInputBlur4 = (e) => {
+        this.setState({
+            begin: e.target.value
+        });
+        console.log('Начало отпуска:', e.target.value);
+    };
+    
+    handleInputBlur5 = (e) => {
+        this.setState({
+            end: e.target.value
+        });
+        console.log('Конец отпуска:', e.target.value);
+    };
+    
+    handleInputBlur6 = (e) => {
+        this.setState({
+            term: e.target.value
+        });
+        console.log('Срок:', e.target.value);
+    };
+    
+    handleInputBlur7 = (e) => {
+        this.setState({
+            date: e.target.value
+        });
+        console.log('Дата заявления:', e.target.value);
+    };
+
     render() {
         return (        
             <form className='form'>
@@ -23,51 +66,44 @@ class Form extends React.Component {
                 <input      
                     type='text'
                     className='input' 
-                    ref='surname'
-                    onBlur={this.update.bind(this)}
+                    onBlur={this.handleInputBlur1}
                 /> {this.state.surname}
                 <label className='form__label'>Имя</label>
                 <input 
                     type='text' 
                     className='input' 
-                    ref='name'
-                    onBlur={this.update.bind(this)}
+                    onBlur={this.handleInputBlur2}
                 /> {this.state.name}
                 <label className='form__label'>Отчество</label>
                 <input 
                     type='text' 
                     className='input' 
-                    ref='secondname'
-                    onBlur={this.update.bind(this)}
+                    onBlur={this.handleInputBlur3}
                 /> {this.state.secondname}
                 <label className='form__label'>Начало отпуска</label>
                 <input 
                     type='date' 
                     className='input' 
-                    ref='begin'
-                    onBlur={this.update.bind(this)}
+                    onBlur={this.handleInputBlur4}
                 /> {this.state.begin}
                 <label className='form__label'>Окончание отпуска</label>
                 <input 
                     type='date' 
                     className='input' 
-                    ref='end'
-                    onBlur={this.update.bind(this)}
+                    onBlur={this.handleInputBlur5}
                 /> {this.state.end}
                 <label className='form__label'>Срок</label>
                 <input 
                     type='number' 
                     className='input' 
                     size="3" 
-                    ref='term'
-                    onBlur={this.update.bind(this)}
+                    onBlur={this.handleInputBlur6}
                 /> {this.state.term}
                 <label className='form__label'>Дата заявления</label>
                 <input 
                     type='date' 
                     className='input' 
-                    ref='date'
-                    onBlur={this.update.bind(this)}
+                    onBlur={this.handleInputBlur7}
                 /> {this.state.date}
             </form>
         );
