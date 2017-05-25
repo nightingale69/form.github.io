c7a4160f48572f0861c3e8fe9407eb1cf8496bc3
nightingale69 <jaccobdasouza@gmail.com>
2017-05-25 22:09:03 +0300

    Added the file 'app.css'  in 'app.js'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 //import Document from './Components/document.js';
 import Form from './Components/form.js';
+import './Components/Styles/app.css';
       
 class App extends React.Component {
   render(){
