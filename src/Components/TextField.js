12b2c4a9ed7b8278454b60213632470aab57867d
nightingale69 <jaccobdasouza@gmail.com>
2017-06-03 08:54:19 +0300

    the component for the handler has been edited

--- src/Components/TextField.js
+++ src/Components/TextField.js
@@ -2,8 +2,26 @@ import React from 'react';
 import '../styles/textfield.css';
 
 class TextField extends React.Component {
+    handleBlur = (e) => {
+        const value = e.target.value;
+
+        this.props.onBlur(value);
+    };
+
     render() {
-        return <input ref='input' type='text' onBlur={this.props.handleSurnameBlur}/>
+        const { type, size, label } = this.props;
+
+        return (
+            <div className="text-field">
+                <label className="text-field__label">{label}</label>
+                <input
+                className="text-field__input"
+                type={type}
+                size={size}
+                onBlur={this.handleBlur}
+                />
+            </div>
+        );
     }
 }
     
