4d3355c8b85134005effdd10b65e5757cd608f17
nightingale69 <jaccobdasouza@gmail.com>
2017-06-16 22:15:04 +0300

    array in render

--- src/Components/form.js
+++ src/Components/form.js
@@ -15,49 +15,49 @@ class Form extends React.Component {
             date: ""
         };
     }
-    
+
     handleSurnameBlur = (value) => {
         this.setState({
             surname: value
         });
         console.log("Фамилия:", value);
     };
-    
+
     handleNameBlur = (value) => {
         this.setState({
             name: value
         });
         console.log("Имя:", value);
     };
-    
+
     handleSecondnameBlur = (value) => {
         this.setState({
             secondname: value
         });
         console.log("Отчество:", value);
     };
-        
+
     handleVocationStartBlur = (value) => {
         this.setState({
             vocationStart: value
         });
         console.log("Начало отпуска:", value);
     };
-    
+
     handleVocationEndBlur = (value) => {
         this.setState({
             vocationEnd: value
         });
         console.log("Конец отпуска:", value);
     };
-    
+
     handleTermBlur = (value) => {
         this.setState({
             term: value
         });
         console.log("Срок:", value);
     };
-    
+
     handleDateBlur = (value) => {
         this.setState({
             date: value
@@ -65,65 +65,59 @@ class Form extends React.Component {
         console.log("Дата заявления:", value);
     };
 
-    var fields = [
-        {
-        surname: "",
-        label: "Фамилия", 
-        type: "text",
-        onBlur: "this.handleSurnameBlur"
-        },
-        
-        {
-        name:"",
-        label: "Имя", 
-        type: "text",
-        onBlur: "this.handleNameBlur"
-        },
-            
-        {
-        secondname:"",
-        label: "Отчество", 
-        type: "text",
-        onBlur: "this.handleSecondnameBlur"
-        },
-            
-        {
-        vocationStart:"",
-        label: "Начало отпуска", 
-        type: "date",
-        onBlur: "this.handleVocationStartBlur"
-        },
-            
-        {
-        vocationEnd:"",
-        label: "Конец отпуска", 
-        type: "date",
-        onBlur: "this.handleVocationEndBlur"
-        },
-            
-        {
-        term:"",
-        label: "Срок", 
-        type: "number",
-        size: "2",
-        onBlur: "this.handleTermBlur"
-        },
-            
-        {
-        date:"",
-        label: "Дата заявления", 
-        type: "date",
-        onBlur: "this.handleDateBlur"
-        }
-    ];
-
     render() {
-        return (        
+        const data = {
+            fields: [
+                {
+                label: "Фамилия",
+                type: "text",
+                onBlur: "this.handleSurnameBlur"
+                },
+                {
+                name: "",
+                label: "Имя",
+                type: "text",
+                onBlur: "this.handleNameBlur"
+                },
+                {
+                secondname: "",
+                label: "Отчество",
+                type: "text",
+                onBlur: "this.handleSecondnameBlur"
+                },
+                {
+                vocationStart: "",
+                label: "Начало отпуска",
+                type: "date",
+                onBlur: "this.handleVocationStartBlur"
+                },
+                {
+                vocationEnd: "",
+                label: "Конец отпуска",
+                type: "date",
+                onBlur: "this.handleVocationEndBlur"
+                },
+                {
+                term: "",
+                label: "Срок",
+                type: "number",
+                size: "2",
+                onBlur: "this.handleTermBlur"
+                },
+                {
+                date: "",
+                label: "Дата заявления",
+                type: "date",
+                onBlur: "this.handleDateBlur"
+                }
+            ]
+        };
+        return (
             <form className='form'>
-                <TextField { type, size, label } = {fields.map} />
+                fields.map((item, index) => <TextField key={index} label={item.label} type={item.type} size={item.size}/>)
             </form>
         );
-    }
+    };
 }
 
 export default Form
