71e3860f4430e0dcd0d577d2c52983b3cc3f805d
nightingale69 <jaccobdasouza@gmail.com>
2017-06-27 23:46:05 +0300

    redact App

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from 'react';
 //import Document from './Components/document.js';
 import Form from './Components/Form.js';
 import './styles/app.css';
-      
+
 class App extends React.Component {
-  render() {
-    return (
-      <div className='app__form'>
-        <Form />
-      </div>
-    )
-  }
-}    
-      
-export default App
+	render() {
+		return (
+			<div className="app__form">
+				<Form />
+			</div>
+		);
+	}
+}
+
+export default App;
