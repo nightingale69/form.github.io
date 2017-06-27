545f48a5dc8fd0d6857007a700555372120e7a70
nightingale69 <jaccobdasouza@gmail.com>
2017-06-27 23:47:58 +0300

    adjusted "" and ''

--- src/Components/TextField.js
+++ src/Components/TextField.js
@@ -2,27 +2,27 @@ import React from 'react';
 import '../styles/textfield.css';
 
 class TextField extends React.Component {
-    handleBlur = (e) => {
+  handleBlur = (e) => {
         const value = e.target.value;
 
         this.props.onBlur(value);
     };
 
-    render() {
-        const { type, size, label } = this.props;
+	render() {
+		const { type, size, label } = this.props;
 
-        return (
-            <div className="text-field">
-                <label className="text-field__label">{label}</label>
-                <input
-                className="text-field__input"
-                type={type}
-                size={size}
-                onBlur={this.handleBlur}
-                />
-            </div>
-        );
-    }
+		return (
+			<div className="text-field">
+				<label className="text-field__label">{label}</label>
+				<input
+					className="text-field__input"
+					type={type}
+					size={size}
+					onBlur={this.handleBlur}
+				/>
+			</div>
+		);
+	}
 }
 
-export default TextField
+export default TextField;
