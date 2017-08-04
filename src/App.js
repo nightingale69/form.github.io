ffac3b6025e32575be0b56252fc962b0fac3a4a7
nightingale69 <jaccobdasouza@gmail.com>
2017-08-04 23:04:36 +0300

    add states

--- src/App.js
+++ src/App.js
@@ -4,12 +4,41 @@ import Form from './Components/Form.js';
 import './styles/app.css';
 
 class App extends React.Component {
-	handleFormChange = (idField, value) => {};
+	constructor() {
+		super();
+		this.state = {
+			surname: '________',
+			name:'________',
+			secondname:'________',
+			vocationStart:'________',
+			vocationEnd:'________',
+			term:'________',
+			date:'________'
+		}
+	}
+
+	handleFormChange = (idField, value) => {
+		this.setState({
+			surname: value,
+			name: value,
+			secondname: value,
+			date: value,
+			term: value
+		})
+		console.log(idField, value);
+	}
 
 	render() {
 		return (
 			<div className="app__form">
 				<Form onChange={this.handleFormChange}/>
+				<Document
+					surname={this.state.surname}
+					name={this.state.name}
+					secondname={this.state.secondname}
+					date={this.state.date}
+					term={this.state.term}
+				/>
 			</div>
 		);
 	}
