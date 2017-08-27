b1a0d529b80c7c1a398c55c0db140117f826a2da
nightingale69 <jaccobdasouza@gmail.com>
2017-08-27 23:07:04 +0300

    add "switch"

--- src/App.js
+++ src/App.js
@@ -17,91 +17,76 @@ class App extends React.Component {
 		}
 	};
 
-
-
 	handleFormChange = (idField, value) => {
 		const validation = this.refs.valid;
-
-		if (idField==='1') {
-			this.setState({surname: value});
-			localStorage.setItem('surname', value);
-			validation.innerText = ''
-			if (value.length <= 1) {
-				this.setState({surname: ''});
-				localStorage.setItem('surname', '');
-				validation.innerText = 'Не заполнено поле "Фамилия" или менее 2 символов'
-			} else if (!value.match(/[а-яА-ЯёЁ]/g)) {
-				this.setState({surname: ''});
-				localStorage.setItem('surname', '');
-				validation.innerText = 'При вводе используется только кириллица'
-			}
-		} else if (idField==='2') {
-			this.setState({name: value});
-			localStorage.setItem('name', value);
-			validation.innerText = ('')
-			if (value.length <= 1) {
-				this.setState({name: ''});
-				localStorage.setItem('name', '');
-				validation.innerText = ('Не заполнено поле "Имя" или менее 2 символов')
-			} else if (!value.match(/[а-яА-ЯёЁ]/g)) {
-				this.setState({name: ''});
-				localStorage.setItem('name', '');
-				validation.innerText = ('При вводе используется только кириллица')
-			}
-		} else if (idField==='3') {
-			this.setState({secondname: value});
-			localStorage.setItem('secondname', value);
-			validation.innerText = ('')
-			if (value.length <= 1) {
-				this.setState({secondname: ''});
-				localStorage.setItem('secondname', '');
-				validation.innerText = ('Не заполнено поле "Отчество" или менее 2 символов')
-			} else if (!value.match(/[а-яА-ЯёЁ]/g)) {
-				this.setState({secondname: ''});
-				localStorage.setItem('secondname', '');
-				validation.innerText = ('При вводе используется только кириллица')
-			}
-		} else if (idField==='4') {
-			this.setState({vocationStart: value});
-			localStorage.setItem('vocationStart', value);
-			validation.innerText = ('')
-			if (value.length >= 11) {
-				this.setState({vocationStart: ''});
-				localStorage.setItem('vocationStart', '');
-				validation.innerText = ('Неправильный год "Начало отпуска"')
-			}
-		} else if (idField==='5'){
-			this.setState({vocationEnd: value});
-			localStorage.setItem('vocationEnd', value);
-			validation.innerText = ('')
-			if (value.length >= 11) {
-				this.setState({vocationEnd: ''});
-				localStorage.setItem('vocationEnd', '');
-				validation.innerText = ('Неправильный год "Конец отпуска"')
-			}
-		} else if (idField==='6'){
-			this.setState({term: value});
-			localStorage.setItem('term', value);
-			validation.innerText = ('')
-			if (value.length === 0) {
-				this.setState({term: ''});
-				localStorage.setItem('term', '');
-				validation.innerText = ('Не заполнено поле "Срок"')
-			} else if (value.length >= 3) {
-				this.setState({term: ''});
-				localStorage.setItem('term', '');
-				validation.innerText = ('Вводимое значение не более 99')
-			}
-		} else if (idField==='7'){
-			this.setState({date: value});
-			localStorage.setItem('date', value);
-			validation.innerText = ('')
-			if (value.length >= 11) {
-				this.setState({date: ''});
-				localStorage.setItem('date', '');
-				validation.innerText = ('Неправильный год в "Дата"')
-			}
-		}
+		switch (idField) {
+			case '1':
+				if (value.length <= 1) {
+					validation.innerText = 'Не заполнено поле "Фамилия" или менее 2 символов'
+				} else if (!value.match(/[а-яА-ЯёЁ]/g)) {
+					validation.innerText = 'При вводе используется только кириллица'
+				} else {
+					this.setState({surname: value});
+					localStorage.setItem('surname', value)
+				};
+				break;
+			case '2':
+				if (value.length <= 1) {
+					validation.innerText = 'Не заполнено поле "Имя" или менее 2 символов'
+				} else if (!value.match(/[а-яА-ЯёЁ]/g)) {
+					validation.innerText = 'При вводе используется только кириллица'
+				} else {
+					this.setState({name: value});
+					localStorage.setItem('name', value)
+				};
+				break;
+			case '3':
+				if (value.length <= 1) {
+					validation.innerText = 'Не заполнено поле "Отчество" или менее 2 символов'
+				} else if (!value.match(/[а-яА-ЯёЁ]/g)) {
+					validation.innerText = 'При вводе используется только кириллица'
+				} else {
+					this.setState({secondname: value});
+					localStorage.setItem('secondname', value)
+				};
+				break;
+			case '4':
+				if (value.length >= 11) {
+					validation.innerText = ('Неправильный год "Начало отпуска"')
+				} else {
+					this.setState({vocationStart: value});
+					localStorage.setItem('vocationStart', value)
+				};
+				break;
+			case '5':
+				if (value.length >= 11) {
+					validation.innerText = ('Неправильный год "Конец отпуска"')
+				} else {
+					this.setState({vocationEnd: value});
+					localStorage.setItem('vocationEnd', value)
+				};
+				break;
+			case '6':
+				if (value.length === 0) {
+					validation.innerText = ('Не заполнено поле "Срок"')
+				} else if (value.length >= 3) {
+					validation.innerText = ('Вводимое значение не более 99')
+				} else {
+					this.setState({term: value});
+					localStorage.setItem('term', value)
+				};
+				break;
+			case '7':
+				if (value.length >= 11) {
+					validation.innerText = ('Неправильный год в "Дата"')
+				} else {
+					this.setState({date: value});
+					localStorage.setItem('date', value)
+				};
+				break;
+			default:
+				break;
+		};
 	};
 
 	render() {
